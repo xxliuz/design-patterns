1bb14f57a074ebcef2345e57c42bb9d0887937b1
zhou.liu <zhou.liu@united-imaging.com>
2022-04-20 10:57:15 +0800

    饿汉式单例

--- singleton/src/main/java/com/zhou/singleton/IvoryTower.java
+++ singleton/src/main/java/com/zhou/singleton/IvoryTower.java
@@ -9,7 +9,11 @@ package com.zhou.singleton;
 public final class IvoryTower {
 
     /**
-     * 私有构造函数，因此没有人可以实例化该类
+     * 饿汉式
+     * 类加载到内存后，就实例化一个单例，JVM保证线程安全
+     * 唯一缺点：不管用到与否，类装载时就完成实例化
+     * Class.forName("")
+     * （话说你不用的，你装载它干啥）
      */
     private IvoryTower(){
 
